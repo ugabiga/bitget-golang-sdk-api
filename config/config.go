aeb8cb0f658c6769f8ce001ecb9667d3d4d4c051
Claudio Giampaolo <claudio.giampaolo@gmail.com>
2022-11-16 21:10:16 +0100

    bitget API SDK repack - go 1.16

---
+++ config/config.go
@@ -0,0 +1,27 @@
+package config
+
+import "os"
+
+const (
+	//restApi config
+	BaseUrl       = "https://api.bitget.com"
+	TimeoutSecond = 30
+
+	//websocket config
+	WsUrl = "wss://ws.bitgetapi.com/spot/v1/stream"
+)
+
+// credentials
+type ApiCreds struct {
+	ApiKey string
+	SecretKey string
+	PASSPHRASE string
+}
+
+func Get() *ApiCreds {
+	return &ApiCreds{
+		ApiKey:     os.Getenv("BITGET_API_KEY"),
+		SecretKey:  os.Getenv("BITGET_API_SECRET"),
+		PASSPHRASE: os.Getenv("BITGET_API_PASS"),
+	}
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module bitget
+
+go 1.16
+
+require (
+	github.com/goccy/go-json v0.9.7
+	github.com/gorilla/websocket v1.4.2
+	github.com/robfig/cron v1.2.0
+	go.uber.org/zap v1.19.1
+
+)
